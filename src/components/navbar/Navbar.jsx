8849727986cb4737a0e33866e5d10de2090836ea
ulus <uluskk@gmail.com>
2023-11-10 14:09:29 +0100

    links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,9 +7,18 @@ function Navbar() {
       <div className="wrapper">
         <span>Ulus Karakoc</span>
         <div className="social">
-          <a href=""> <img src="./xing.png" alt="" />{" "} </a>
-          <a href=""> {" "}   <img src="./linkedin.png" alt="" /> </a>
-          <a href="">   <img className="github" src="./github.png" alt="" /> </a>
+          <a href="https://www.xing.com/profile/Ulus_Karakoc">
+            {" "}
+            <img src="./xing.png" alt="" />{" "}
+          </a>
+          <a href="https://www.linkedin.com/in/ulus-karako%C3%A7-7a7b101b0/">
+            {" "}
+            <img src="./linkedin.png" alt="" />{" "}
+          </a>
+          <a href="https://github.com/uluskarakoc">
+            {" "}
+            <img className="github" src="./github.png" alt="" />{" "}
+          </a>
         </div>
       </div>
     </div>
