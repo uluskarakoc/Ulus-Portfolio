bd74e20d14fc2cad26e69f9354c4db370c89412e
ulus <uluskk@gmail.com>
2023-11-15 20:37:31 +0100

    motion

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,31 +1,27 @@
 import "./about.scss";
+import { motion } from "framer-motion";
 
 function About() {
   return (
     <div className="about">
-        <h1 className="text">About Me</h1>
+      <h1 className="text">About Me</h1>
       <div className="container">
-        <div className="meText">
+        <motion.div whileHover={{ scale: 1.2 }} className="meText">
           <h1>Hi ich bin ULUS,</h1>
           <p>
-            Ich bin ein ausgebildeter Full Stack Web Entwickler mit einer
-            Leidenschaft für das Programmieren. Bevor ich meine Weiterbildung in
-            Deutschland abgeschlossen habe, habe ich bereits eigenständig mit
-            dem Erlernen des Programmierens begonnen und dabei meine Fähigkeiten
-            kontinuierlich ausgebaut. Mit 2,5 Jahren Erfahrung als Lehrer in der
-            Türkei und Kirgisistan konnte ich meine kommunikativen Fähigkeiten
-            entwickeln und schätze die Bedeutung einer strukturierten
-            Herangehensweise an Aufgaben. Diese Erfahrungen kombiniere ich nun
-            mit meiner Leidenschaft für die Technologiebranche, um innovative
-            Lösungen zu entwickeln. Ich bin offen für neue Herausforderungen,
-            teamorientiert und stets bereit, mich weiterzuentwickeln. Ich freue
-            mich auf die Möglichkeit, an neuen Projekten zu arbeiten und meine
-            Fähigkeiten als Web Entwickler weiter zu vertiefen. Vielen Dank!
+            Ich habe mein Hobby, Photoshop und Fotografie, das ich in meinen
+            20ern begonnen habe, durch einen einjährigen Weiterbildungskurs
+            erfolgreich abgeschlossen und bin jetzt als Full Stack Webentwickler
+            an einem schönen Punkt angelangt. Mit dem Spielen von Stil, Farben
+            und Animationen einer Webseite sowie dem Verarbeiten der an die
+            Backend gesendeten Informationen, um sie dem Benutzer nahtlos
+            zurückzugeben, empfinde ich eine Freude, die vergleichbar ist mit
+            dem stundenlangen Betrachten eines Gemäldes durch einen Maler.
           </p>
-        </div>
-        <div className="image">
-          <img src="./people.webp" alt="" />
-        </div>
+        </motion.div>
+        <motion.div className="image">
+          <img src="./code2.jpg" alt="" />
+        </motion.div>
       </div>
     </div>
   );
